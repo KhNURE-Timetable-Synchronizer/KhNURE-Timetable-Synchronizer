622a44774a8c4efdf54203161741206ce7cbb180
Дмитро Кузьменко <dmytro.kuzmenko@nure.ua>
2024-04-29 23:15:58 +0300

    KTS-45 Update GET endpoint /api/v1/timetables/google (KTS-38)

--- backend/src/main/java/com/khnure/timetable/synchronizer/mapper/GoogleCalendarDtoMapper.java
+++ backend/src/main/java/com/khnure/timetable/synchronizer/mapper/GoogleCalendarDtoMapper.java
@@ -3,8 +3,10 @@ package com.khnure.timetable.synchronizer.mapper;
 import com.khnure.timetable.synchronizer.dto.GoogleCalendarDto;
 import com.khnure.timetable.synchronizer.model.GoogleCalendar;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface GoogleCalendarDtoMapper {
+     @Mapping(source = "calendarId", target = "id")
      GoogleCalendarDto toDto(GoogleCalendar googleCalendar);
 }
