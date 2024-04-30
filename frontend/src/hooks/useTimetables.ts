b94b482a9e7760722f3021e3b6e5eb84d3080aeb
Denys Bondarenko <denbon04@gmail.com>
2024-04-30 14:34:15 +0200

    KTS-41 timetables hook and minimal read-only ui

---
+++ frontend/src/hooks/useTimetables.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query"
+
+export const timetablesQueryKey = ["timetables"]
+export type TimetableShort = { id: number; name: string }
+
+const fetchTimetables = async () => {
+  const res = await fetch(
+    `${import.meta.env.VITE_BACKEND_URL}/api/v1/khnure/timetables`
+  )
+  if (!res.ok) {
+    throw new Error("GET Timetables response was not ok")
+  }
+  const data = await res.json()
+  return data as TimetableShort[]
+}
+
+export default function useTimetables() {
+  const { data, isError, isLoading, status } = useQuery({
+    queryKey: timetablesQueryKey,
+    queryFn: fetchTimetables,
+  })
+
+  return { timetables: data, status, isError, isLoading }
+}
--- frontend/src/routes/_auth/index.tsx
+++ frontend/src/routes/_auth/index.tsx
@@ -1,26 +1,60 @@
-import { useQuery } from "@tanstack/react-query"
 import { createFileRoute } from "@tanstack/react-router"
+import useTimetables from "../../hooks/useTimetables"
+import { Trash2Icon } from "lucide-react"
 
 export const Route = createFileRoute("/_auth/")({
   component: Home,
 })
 
 function Home() {
-  const res = useQuery({
-    queryKey: ["timetables"],
-    queryFn: () => {
-      return fetch(
-        `${import.meta.env.VITE_BACKEND_URL}/api/v1/khnure/timetables`
-      ).then(res => res.json())
-    },
-  })
-
-  console.log({ res })
+  const { timetables, isLoading } = useTimetables()
 
   return (
-    <div className="p-2">
-      <p>Hello from Home!</p>
-      <pre>{JSON.stringify(res.data, null, 2)}</pre>
+    <div className="flex gap-2">
+      <div className="space-y-4 w-64">
+        {isLoading ? (
+          <div className="flex justify-center">
+            <span className="loading loading-spinner loading-md" />
+          </div>
+        ) : (
+          <table className="table table-sm">
+            <tbody>
+              {timetables?.map(timetable => (
+                <tr key={timetable.id} className="hover">
+                  <td>{timetable.name}</td>
+                  <td className="w-6">
+                    <button
+                      className="btn btn-xs hover:!text-white btn-outline btn-error btn-square"
+                      onClick={() =>
+                        (
+                          document.getElementById(
+                            `delete-tt-modal-${timetable.id}`
+                          ) as any
+                        ).showModal()
+                      }
+                    >
+                      <Trash2Icon size={14} />
+                    </button>
+                    <dialog
+                      id={`delete-tt-modal-${timetable.id}`}
+                      className="modal"
+                    >
+                      <div className="modal-box">
+                        <h3 className="font-bold text-lg">Hello!</h3>
+                        <p className="py-4">
+                          Press ESC key or click outside to close
+                        </p>
+                      </div>
+                    </dialog>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+        <button className="btn btn-primary btn-sm w-full">Add Timetable</button>
+      </div>
+      <div className="flex-1 bg-orange-300" />
     </div>
   )
 }
