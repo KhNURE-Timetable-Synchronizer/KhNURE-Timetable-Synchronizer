2c9590c407960efff348acf9430d8c69c0488eae
Denys Bondarenko <denbon04@gmail.com>
2024-05-07 11:32:54 +0200

    KTS-37: done with backend

--- frontend/src/routes/_auth/index.tsx
+++ frontend/src/routes/_auth/index.tsx
@@ -13,45 +13,30 @@ export const Route = createFileRoute("/_auth/")({
   validateSearch: rootParamsSchema,
 })
 
-const d = [
-  {
-    id: 1,
-    name: "knt",
-    googleId:
-      "c_a5db7533367412f08e9db638faed8f6d0062d06ca65bcb174c9fa68fe6ce6597@group.calendar.google.com",
-  },
-  {
-    id: 2,
-    name: "knt",
-    googleId: "ru.ukrainian#holiday@group.v.calendar.google.com",
-  },
-]
-
 function Home() {
-  // const { personalTimetables } = useTimetables()
-  const personalTimetables = { isLoading: false, data: d }
+  const { personalTimetables: timetables } = useTimetables()
   const { id } = Route.useSearch()
 
   const calendarId = id
-    ? personalTimetables.data?.find(timetable => timetable.id === id)?.googleId
-    : personalTimetables.data?.[0]?.googleId
+    ? timetables.data?.find(timetable => timetable.id === id)?.googleCalendarId
+    : timetables.data?.[0]?.googleCalendarId
 
   return (
     <div className="flex gap-2 flex-col lg:flex-row">
       <div className="space-y-4 w-64">
-        {personalTimetables.isLoading ? (
+        {timetables.isLoading ? (
           <div className="flex justify-center">
             <span className="loading loading-spinner loading-md" />
           </div>
         ) : (
           <table className="table table-xs">
             <tbody>
-              {personalTimetables.data?.map((timetable, i) => (
+              {timetables.data?.map((timetable, i) => (
                 <tr key={timetable.id} className="hover">
                   <td>
                     <Link
                       search={{ id: i == 0 ? undefined : timetable.id }}
-                      className="btn btn-link"
+                      className="btn btn-link btn-sm"
                     >
                       {timetable.name}
                     </Link>
@@ -66,12 +51,14 @@ function Home() {
         )}
         <AddTimetableButton />
       </div>
-      {calendarId && (
+      {calendarId ? (
         <iframe
           src={`https://calendar.google.com/calendar/embed?src=${encodeURIComponent(calendarId)}&ctz=Europe%2FKiev`}
           height="600"
           className="lg:flex-1 w-full"
         />
+      ) : (
+        <p className="place-self-center ml-4">{"<"}- Select timetable</p>
       )}
     </div>
   )
