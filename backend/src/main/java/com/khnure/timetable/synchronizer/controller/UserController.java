763993c1c829c6e379652d30869c63501bc3c3fa
Дмитро Кузьменко <dmytro.kuzmenko@nure.ua>
2024-05-15 12:07:55 +0300

    KTS-62 Create GET endpoint /api/v1/users?page=&pageSize=

--- backend/src/main/java/com/khnure/timetable/synchronizer/controller/UserController.java
+++ backend/src/main/java/com/khnure/timetable/synchronizer/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.khnure.timetable.synchronizer.controller;
 
-import com.khnure.timetable.synchronizer.dto.ScheduleDto;
 import com.khnure.timetable.synchronizer.dto.UserDto;
 import com.khnure.timetable.synchronizer.mapper.UserMapper;
 import com.khnure.timetable.synchronizer.model.User;
@@ -8,9 +7,9 @@ import com.khnure.timetable.synchronizer.service.UserService;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,15 +21,15 @@ import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("${api.base-url}")
+@Validated
 public class UserController {
     private final UserService userService;
-    @Autowired
-    private UserMapper userMapper;
+    private final UserMapper userMapper;
 
-    @GetMapping( "/users")
+    @GetMapping("/users")
     public ResponseEntity<?> getUsersWithPagination(
-            @RequestParam(value = "page", defaultValue = "1", required = false) @Min(1) Integer page,
-            @RequestParam(value = "pageSize", defaultValue = "20", required = false) @Min(1) @Max(100)   Integer pageSize) {
+            @RequestParam(value = "page", defaultValue = "1", required = false) @Min(value = 1, message = "Page must start from 1") Integer page,
+            @RequestParam(value = "pageSize", defaultValue = "20", required = false) @Min(value = 1, message = "Users per page must be greater 0") @Max(value = 100, message = "Users per page can be max 100") Integer pageSize) {
         Page<User> userPage = userService.getUsers(page - 1, pageSize);
         List<UserDto> response = userPage.getContent().stream()
                 .map(userMapper::toDto)
