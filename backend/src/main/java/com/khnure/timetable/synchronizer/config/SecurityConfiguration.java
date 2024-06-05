fc3558ae965b1c088cce941b5638999162e92357
Stanislav Hlova <stanislav.hlova@gmail.com>
2024-06-05 15:28:07 +0300

    KTS-69 Security fix

--- backend/src/main/java/com/khnure/timetable/synchronizer/config/SecurityConfiguration.java
+++ backend/src/main/java/com/khnure/timetable/synchronizer/config/SecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -49,7 +50,7 @@ public class SecurityConfiguration {
                     urlConfig.requestMatchers(apiVersionPath + "/jwt/create").permitAll()
                             .requestMatchers(WHITE_LIST_URL).permitAll()
                             .requestMatchers(apiVersionPath + "/users").hasRole("ADMIN")
-                            .requestMatchers(apiVersionPath + "/request").hasRole("ADMIN")
+                            .requestMatchers(HttpMethod.GET,apiVersionPath + "/request").hasRole("ADMIN")
                             .requestMatchers(apiVersionPath + "/request/*/status").hasRole("ADMIN")
                             .requestMatchers(apiVersionPath + "/request/?").hasRole("ADMIN")
                             .anyRequest().authenticated();
